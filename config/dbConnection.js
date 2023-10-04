a6c5dad11cf8cd47ec642efa550ad36910628f96
Hajarathu Shamna <shamnahajara@gmail.com>
2023-10-04 12:53:30 +0530

    db connection resolved

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
+
 const MONGOOSE = process.env.MONGOOSE
 
 module.exports.connectDB = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/film-plus', {
+    mongoose.connect(MONGOOSE, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(() => {
-        console.log('Database Connected');
-    })
-    .catch((err) => {
-        console.error('Error connecting to database:', err);
-    });
+        .then(() => {
+            console.log('Database Connected');
+        })
+        .catch((err) => {
+            console.error('Error connecting to database:', err);
+        });
 };
